ea55b6fa9f2729b7e8b9ae6601637dadd45305eb
Michael <mdlevick@gmail.com>
2019-12-06 11:05:14 -0600

    worked on server

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const helmet = require('helmet') 
+const logger = require('./api/logger.js');
+
+const projectRouter = require('./data/helpers/projectRouter.js');
+
+const server = express();
+
+server.use(helmet());
+server.use(express.json()); 
+
+
+
+server.get('/', logger('logger for server.js'), (req, res) => {
+  res.send(`<h2>Target acquired!</h2>`)
+});
+
+// server.use('/api/projects', logger('logger for projects'), projectRouter);
+server.use('/api/projects', projectRouter);
+
+
+
+module.exports = server;
